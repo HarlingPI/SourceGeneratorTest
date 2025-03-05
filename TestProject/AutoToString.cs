09e324103e35ae45a7d4b551e52c088916a3eef6
HarlingPI <BePJaP@outlook.com>
2025-03-05 16:21:32 +0800

    Commit by scripts

---
+++ TestProject/AutoToString.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    /// <summary>
+    /// 作者:   Harling
+    /// 时间:   2025/3/5 15:32:14
+    /// 备注:   此文件通过PIToolKit模板创建
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class AutoToStringAttribute : Attribute
+    {
+
+    }
+}
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,11 +1,20 @@
 ﻿using SourceGeneratorInCSharp;
 namespace TestProject
 {
+    [AutoToString]
+    public partial class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var person = new Person { Name = "John", Age = 30 };
+            Console.WriteLine(person);
 
             HelloWorld.SayHello();
 
